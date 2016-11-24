46d7b6352485fc1f4bc46c83ca10f408563e1167
RomanAlekseev <roman.alekseev555@gaail.com>
2016-11-24 10:47:09 +0200

    add gitignire

--- app/todo.component.js
+++ app/todo.component.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-var core_1 = require("@angular/core");
+var core_1 = require('@angular/core');
 var TodoComponent = (function () {
     function TodoComponent() {
         this.removeItem = new core_1.EventEmitter();
@@ -16,26 +16,26 @@ var TodoComponent = (function () {
     TodoComponent.prototype.deleteItem = function (id) {
         this.removeItem.emit(id);
     };
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', String)
+    ], TodoComponent.prototype, "todoItem", void 0);
+    __decorate([
+        core_1.Input(), 
+        __metadata('design:type', Array)
+    ], TodoComponent.prototype, "todoList", void 0);
+    __decorate([
+        core_1.Output(), 
+        __metadata('design:type', Object)
+    ], TodoComponent.prototype, "removeItem", void 0);
+    TodoComponent = __decorate([
+        core_1.Component({
+            selector: 'todo-component',
+            template: "\n        <div href=\"#\" class=\"list-group-item\" *ngFor=\"let item of todoList; let i = index\">\n            <h4 class=\"list-group-item-heading\">\n                <button type=\"button\" class=\"btn btn-danger\" (click)=\"deleteItem(i)\">x</button>\n                {{item}}\n            </h4>\n        </div>\n    "
+        }), 
+        __metadata('design:paramtypes', [])
+    ], TodoComponent);
     return TodoComponent;
 }());
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", String)
-], TodoComponent.prototype, "todoItem", void 0);
-__decorate([
-    core_1.Input(),
-    __metadata("design:type", Array)
-], TodoComponent.prototype, "todoList", void 0);
-__decorate([
-    core_1.Output(),
-    __metadata("design:type", Object)
-], TodoComponent.prototype, "removeItem", void 0);
-TodoComponent = __decorate([
-    core_1.Component({
-        selector: 'todo-component',
-        template: "\n        <div href=\"#\" class=\"list-group-item\" *ngFor=\"let item of todoList; let i = index\">\n            <h4 class=\"list-group-item-heading\">\n                <button type=\"button\" class=\"btn btn-danger\" (click)=\"deleteItem(i)\">x</button>\n                {{item}}\n            </h4>\n        </div>\n    "
-    }),
-    __metadata("design:paramtypes", [])
-], TodoComponent);
 exports.TodoComponent = TodoComponent;
 //# sourceMappingURL=todo.component.js.map
